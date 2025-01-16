6c81f4ab81d51b0e46d2a0c1adb31bcb906a2324
liulma <linda.ulma@outlook.com>
2025-01-16 16:44:39 +0200

    Update exercise_1.py

--- exercise_1.py
+++ exercise_1.py
@@ -6,39 +6,32 @@ def create_list(input_value):
 
     return list_values
 
-def min_max(input_value):
-    list_values = create_list(input_value)
-
-    min_value = min(list_values)
-    max_value = max(list_values)
+def min_max(data_list):
+    min_value = min(data_list)
+    max_value = max(data_list)
 
     return min_value, max_value
 
-def mean(input_value):
-    list_values = create_list(input_value)
-
-    mean_value = sum(list_values) / len(list_values)
+def mean(data_list):
+    mean_value = sum(data_list) / len(data_list)
 
     return mean_value
 
-def median(input_value):
-    list_values = create_list(input_value)
-
-    median_value = statistics.median(list_values)
+def median(data_list):
+    median_value = statistics.median(data_list)
 
     return median_value
 
-def mode(input_value):
-    list_values = create_list(input_value)
-
-    mode_value = statistics.mode(list_values)
+def mode(data_list):
+    mode_value = statistics.mode(data_list)
 
     return mode_value
 
 
 user_value = input("Please enter values: ")
-min_value, max_value = min_max(user_value)
-mean_value = mean(user_value)
-median_value = median(user_value)
-mode_value = mode(user_value)
+data_list = create_list(user_value)
+min_value, max_value = min_max(data_list)
+mean_value = mean(data_list)
+median_value = median(data_list)
+mode_value = mode(data_list)
 print(f"The minimum value is {min_value} and the maximum value is {max_value}. The mean is {mean_value}, the median is {median_value} and the mode is {mode_value}.")
