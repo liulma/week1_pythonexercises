2894ace61b12538fa492bdc630bff355d063be88
liulma <linda.ulma@skillio.ai>
2025-01-17 13:56:59 +0200

    Some improvements

--- exercise_4.py
+++ exercise_4.py
@@ -1,12 +1,10 @@
 import requests
 
-new_list = []
+items = []
 
-for i in range(1,6):
-    x = requests.get('https://catfact.ninja/fact')
-    data = x.json()
+for i in range(0,5):
+    response = requests.get('https://catfact.ninja/fact')
+    items.append(response.json()["fact"])
 
-    new_list.append(data)
-
-for item in new_list:
+for item in items:
     print(item)
--- exercise_4_1.py
+++ exercise_4_1.py
@@ -1,11 +1,8 @@
 import requests
 import json
 
-x = requests.get('https://api.github.com/search/repositories?q=language:python')
-data = x.json()
+response = requests.get('https://api.github.com/search/repositories?q=language:python')
+data = response.json()
 
-for item in data["items"]:
-    forks = item["forks"]
-    name = item["name"]
-    description = item["description"]
-    print(f"Forks:{forks}. Name:{name} Description:{description}")
+for i in sorted(data['items'], key=lambda x: x['forks'], reverse=True):
+    print(f'Forks:{i["forks"]}. Name:{i["name"]} Description:{i["description"]}')
